e8cc4e1c27fbfbd09200cf3d808b9b314f3f12c8
phoenix <1047767505@qq.com>
2017-07-20 16:35:11 +0800

    读取数据库中的内容

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for counts, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(counts)) + str(post)+"<br>")
+    return HttpResponse(post_lists)
