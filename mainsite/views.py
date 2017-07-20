f3ce19b5ac6cf38ec6e17b4ab05eed457c064721
phoenix <1047767505@qq.com>
2017-07-20 16:43:39 +0800

    修改文章显示

--- mainsite/views.py
+++ mainsite/views.py
@@ -7,5 +7,6 @@ def homepage(request):
     posts = Post.objects.all()
     post_lists = list()
     for counts, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(counts)) + str(post)+"<br>")
+        post_lists.append("No.{}:".format(str(counts)) + str(post)+"<hr>")
+        post_lists.append("<small>" + str(post.body.encode('utf-8')) +"<\small><br><br>")
     return HttpResponse(post_lists)
